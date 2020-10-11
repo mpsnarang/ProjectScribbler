0ac45068e1d774383dd75e2f3573312d40db5921
Mahinder P S Narang <mpsnarang@gmail.com>
2020-10-11 13:42:57 +0530

    JS changes to handle Sign In modal

---
+++ scripts/common.js
@@ -0,0 +1,19 @@
+function displaySignInModal(){
+    var modal = document.getElementById("id-signin-modal");
+    modal.style.display = "flex";
+}
+
+function displaySignUpModal(){
+    var modal = document.getElementById("id-signup-modal");
+    modal.style.display = "flex";
+}
+
+function hideSignInModal(){
+    var modal = document.getElementById("id-signin-modal");
+    modal.style.display = "none";
+}
+
+function hideSignUpModal(){
+    var modal = document.getElementById("id-signup-modal");
+    modal.style.display = "none";
+}
